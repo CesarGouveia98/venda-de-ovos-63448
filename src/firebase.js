6db988de7846d5e4e7b18250aeaf2a8e04d811d0
Cesar <cesar_master_98@hotmail.com>
2026-08-03 22:19:26 +0100

    Fix: Correcting the firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getMessaging } from "firebase/messaging"; // Importa o serviço de mensagens
+import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAneJwJB5xg1oD1dBKOI7y07r7XEM70xQE",
@@ -16,23 +16,4 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const messaging = getMessaging(app); 
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { getMessaging } from "firebase/messaging"; // Importa o serviço de mensagens
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAneJwJB5xg1oD1dBKOI7y07r7XEM70xQE",
-  authDomain: "augovos.firebaseapp.com",
-  projectId: "venda-de-ovos-63448",
-  storageBucket: "venda-de-ovos-63448.appspot.com",
-  messagingSenderId: "973253465115",
-  appId: "1:973253465115:web:3757df3250d290debcd4be"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const messaging = getMessaging(app); 
+export const messaging = getMessaging(app);
